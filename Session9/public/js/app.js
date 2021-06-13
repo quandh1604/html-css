375c2b18ba5b36d37c4221cf1d9baa3302953c4c
quandh1604 <quandh1604@gmail.com>
2021-06-14 02:21:16 +0700

    first commit

--- Session9/public/js/app.js
+++ Session9/public/js/app.js
@@ -3,19 +3,19 @@
 
 // console.log(app.style);
 
-//cho 1 thẻ div chứa nội dung bên trong
-//Thay đổi màu và in đạm nội dung của nó = js
+// // cho 1 thẻ div chứa nội dung bên trong
+// // Thay đổi màu và in đạm nội dung của nó = js
 
 // app.style.color = "aqua"
 // app.style.fontWeight = "bold"
 // app.style.fontSize = "50px"
 
 
-//children, parent
-// const ul = document.getElementById("ul");
-// console.log(ul.children[2].parentElement);
+// children, parent
+const ul = document.getElementById("ul");
+console.log(ul.children[2].parentElement);
 
-//method add, remove, class, id
+// // method add, remove, class, id
 
 // console.log(ul.id);
 // console.log(ul.classList);
@@ -29,7 +29,7 @@
 // console.log(ul.appendChild(li));
 // console.log(ul);
 
-//Cho 1 danh sách việc làm. Thêm nội dung việc mới, thay đổi màu, bc, fontSize của nội dung bằng js
+// // Cho 1 danh sách việc làm. Thêm nội dung việc mới, thay đổi màu, bc, fontSize của nội dung bằng js
 // const work = document.getElementById('work');
 // console.log(document.createElement("li"));
 // const li = document.createElement("li");
@@ -39,7 +39,7 @@
 // work.style.color = "aqua";
 // work.style.fontSize = "50px";
 
-//event, click
+// event, click
 
 const btn = document.getElementById("btn");
 btn.onclick = () =>{
@@ -53,8 +53,8 @@ btn.onclick = () =>{
     console.log(ul.appendChild(li));
 
 }
-// Click btn add => Cho người dùng nhập vào title
-//Nhập xong thì add vào list danh sách
+// // Click btn add => Cho người dùng nhập vào title
+// //Nhập xong thì add vào list danh sách
 const btnDelete = document.getElementById("btnDelete");
 btnDelete.onclick = () =>{
     for (let i = 0; i < (ul.children).length; i--) {
@@ -70,3 +70,51 @@ btnDelete.onclick = () =>{
         
 //     }
 // }
+
+function hideShowClick() {
+    let box1 = document.getElementById("box1");
+    if (box1.style.display === "none") {
+        box1.style.display = "block";
+    }else{
+        box1.style.display = "none";
+    }
+}
+
+
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  function setRandomColor() {
+    $("#box2").css("background-color", getRandomColor());
+  }
+
+
+
+    const contentBox31 = document.getElementById("box31").textContent;
+    const contentBox33 = document.getElementById("box33").textContent;
+  function swapClick() {
+      if (document.getElementById("box31").innerHTML == contentBox31) {
+        document.getElementById("box31").innerHTML = contentBox33;
+        document.getElementById("box33").innerHTML = contentBox31;
+      }else if (document.getElementById("box31").innerHTML == contentBox33){
+        document.getElementById("box31").innerHTML = contentBox31;
+        document.getElementById("box33").innerHTML = contentBox33;
+      }
+    
+  }
+
+
+    // const box4 = document.getElementById("box4");
+    var computedFontSize = window.getComputedStyle(document.getElementById("box4")).fontSize;
+    let reset = parseFloat(computedFontSize);
+    // console.log(computedFontSize);
+  function getFontSize() {
+      box4.style.fontSize = (reset + 1) + "px";
+   
+  }
